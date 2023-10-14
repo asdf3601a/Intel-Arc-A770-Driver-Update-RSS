765c2a301d5821d4ece2bb9a8e52be80bf1247f7
cute_panda <asdf3601a@gmail.com>
2023-10-14 17:47:25 +0800

    cache api

--- src/index.js
+++ src/index.js
@@ -13,44 +13,55 @@ export default {
     const url = "https://www.intel.com/content/www/us/en/download/785597/intel-arc-iris-xe-graphics-windows.html";
     const site = await fetch(url);
 
-    var feed = {
-      version: "https://jsonfeed.org/version/1",
-      title: "Intel® Arc™ A770 Drivers",
-      home_page_url: url,
-      items: []
-    }
+    const cache = caches.default;
+    const cache_key = request.url;
+    let response = await cache.match(cache_key);
 
-    var item = "";
+    if (!response) {
+      var item = "";
+      var feed = {
+        version: "https://jsonfeed.org/version/1",
+        title: "Intel® Arc™ A770 Drivers",
+        home_page_url: url,
+        items: []
+      }
 
-    await (
-      new HTMLRewriter().on(
-        'select#version-driver-select>option',
+      await (
+        new HTMLRewriter().on(
+          'select#version-driver-select>option',
+          {
+            element: (element) => { item += `https://www.intel.com${element.getAttribute('value')},` },
+            text: (text) => { item += `${text.text.replace(' (Latest)', '')};` },
+          }
+        )
+          .transform(site)
+          .arrayBuffer()
+      )
+
+      item
+        .split(';')
+        .filter(e => e.length > 3)
+        .forEach(
+          e => {
+            let s = e.split(',');
+            feed.items.push({ id: s[1], title: s[1], url: s[0] });
+          }
+        )
+
+      response = new Response(
+        JSON.stringify(feed),
         {
-          element: (element) => { item += `https://www.intel.com${element.getAttribute('value')},` },
-          text: (text) => { item += `${text.text.replace(' (Latest)', '')};` },
+          headers: {
+            'content-type': 'application/json;charset=UTF-8',
+            'cache-control': 's-maxage=300',
+          }
         }
       )
-        .transform(site)
-        .arrayBuffer()
-    )
 
-    item
-    .split(';')
-    .filter(e => e.length > 3)
-    .forEach(
-      e => {
-        let s = e.split(',');
-        feed.items.push({id: s[1], title: s[1], url: s[0]});
-      }
-    )
+      ctx.waitUntil(cache.put(cache_key, response.clone()));
+    }
 
-    return new Response(
-      JSON.stringify(feed),
-      {
-        headers: {
-          'content-type': 'application/json;charset=UTF-8',
-        }
-      }
-    );
+
+    return response
   },
 };

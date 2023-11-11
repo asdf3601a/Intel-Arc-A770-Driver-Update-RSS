7d1895135476ee7374437212b636522d412e6bcb
cute_panda <asdf3601a@gmail.com>
2023-11-11 17:22:03 +0800

    serving only root path

--- src/index.js
+++ src/index.js
@@ -10,44 +10,45 @@
 
 export default {
   async fetch(request, env, ctx) {
-    const cache = caches.default;
-    const cache_key = request.url;
-    let response = await cache.match(cache_key);
+    if (request.url.split('/').slice(3)[0] != '') return new Response('Not found', { status: 404 })
+    const cache = caches.default
+    const cache_key = request.url
+    let response = await cache.match(cache_key)
 
     if (!response) {
-      const url = 'https://www.intel.com/content/www/us/en/download/785597/intel-arc-iris-xe-graphics-windows.html';
-      const site = await fetch(url);
+      const url = 'https://www.intel.com/content/www/us/en/download/785597/intel-arc-iris-xe-graphics-windows.html'
+      const site = await fetch(url)
 
       var feed = {
         version: 'https://jsonfeed.org/version/1',
         title: 'Intel® Arc™ A770 Drivers',
         home_page_url: url,
         items: [],
-      };
+      }
 
       await new HTMLRewriter()
         .on('select#version-driver-select>option', {
           element: (element) => {
-            feed.items.push({ id: '', title: '', url: `https://www.intel.com${element.getAttribute('value')}` });
+            feed.items.push({ id: '', title: '', url: `https://www.intel.com${element.getAttribute('value')}` })
           },
           text: (text) => {
-            feed.items.at(-1).id += `${text.text.replace(' (Latest)', '')}`;
-            feed.items.at(-1).title += `${text.text.replace(' (Latest)', '')}`;
+            feed.items.at(-1).id += `${text.text.replace(' (Latest)', '')}`
+            feed.items.at(-1).title += `${text.text.replace(' (Latest)', '')}`
           },
         })
         .transform(site)
-        .arrayBuffer();
+        .arrayBuffer()
 
       response = new Response(JSON.stringify(feed), {
         headers: {
           'content-type': 'application/json;charset=UTF-8',
           'cache-control': 'public, max-age=300',
         },
-      });
+      })
 
-      ctx.waitUntil(cache.put(cache_key, response.clone()));
+      ctx.waitUntil(cache.put(cache_key, response.clone()))
     }
 
-    return response;
+    return response
   },
-};
+}
